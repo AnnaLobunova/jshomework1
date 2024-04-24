f032731bb7c194ec30e6c5e3a7623d31ccad4b7d
AnnaLobunova <alobunova@gmail.com>
2024-04-24 13:01:12 +0300

    js changed

---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict"
+const width=5;
+const length=10;
+const halfPerimeter=width+length;
+const diff=length-width;
+const square=width*length;
+const times=length/width;
+const exponent=Math.pow(length, width)
+console.log(halfPerimeter, diff, square, times, exponent);
